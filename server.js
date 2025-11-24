e9a54bf0283b959a4ce027369f0b19d62a91c38c
Brian_PC <s1296027@live.hkmu.edu.hk>
2025-11-24 23:40:14 +0800

    new middleware

--- server.js
+++ server.js
@@ -11,6 +11,12 @@ const fsPromises = require('fs').promises;
 
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
+app.use(session({
+  secret: 'tHiSiSasEcRetStr',  // Replace with your session secret
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: true } // Set to true if using HTTPS
+}));
 app.use(fileUpload()); // Middleware for handling file uploads
 app.use(express.static(path.join(__dirname, 'public')));
 
